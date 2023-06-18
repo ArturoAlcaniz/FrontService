7e78e37362f7800538768c39615141ac57ff427f
ArturoAlcaniz <arturoguijarro@hotmail.com>
2023-06-18 16:50:50 +0000

    Added total price cart

--- components/Commons/ShoppingCartView.tsx
+++ components/Commons/ShoppingCartView.tsx
@@ -2,10 +2,22 @@ export default function ShoppingCartView(thisComponent) {
     let languageSelected = thisComponent.state.languageSelected
     let obtainTextTranslated = thisComponent.translations[languageSelected]
 
+    const totalPrice: number = thisComponent.state.productsToBuy.reduce((acc: number, item: {price: number}) => {
+        return acc + item.price;
+    }, 0);
+
     return(
         <div tabIndex={0} ref={thisComponent.notificationViewRef} id="shoppingCartView" className="box customBox">
             <div className="shoppingCartTitleContainer">
                 <div className="shoppingCartTitle">{obtainTextTranslated["titles"]["cesta"]}</div>
+                <div className="shopping-cart" style={{display: 'block'}}>
+                    <div className="shopping-cart-header">
+                        <div className="shopping-cart-total">
+                            <span className="lighter-text">Total:</span>
+                            <span className="main-color-text">{totalPrice}</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
