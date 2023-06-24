9c3743fb74a2cab0aee46f00ab97f793d22085b1
ArturoAlcaniz <arturoguijarro@hotmail.com>
2023-06-24 19:26:31 +0000

    Improved styles shopping cart

--- components/Commons/ShoppingCartView.tsx
+++ components/Commons/ShoppingCartView.tsx
@@ -23,27 +23,28 @@ export default function ShoppingCartView(thisComponent) {
         <div tabIndex={0} ref={thisComponent.notificationViewRef} id="shoppingCartView" className="box customBox shopping-cart">
             <div className="shoppingCartTitleContainer">
                 <div className="shoppingCartTitle">{obtainTextTranslated["titles"]["cesta"]}</div>
-                <div className="shopping-cart" style={{display: 'block'}}>
-                    <div className="shopping-cart-header">
-                        <div className="shopping-cart-total">
-                            <span className="lighter-text">Total: </span>
-                            <span className="main-color-text">{Number(totalPrice)}</span>
-                        </div>
-                    </div>
-                </div>
             </div>
             <ul className="shopping-cart-items">
                 {thisComponent.state.productsToBuy && thisComponent.state.productsToBuy.length>0 && thisComponent.state.productsToBuy.map(product => {
                     return (
-                        <li key={product.id} className="clearfix">
-                            <Image width={70} height={70} src={product.image ? `/api/products/image/${product.image}` : `/api/products/image`} alt="Product Image"/>
-                            <span className="item-name">{product.productname}</span>
-                            <span className="item-price">{product.price}</span>
-                            <i onClick={() => {deleteProductToBuy(product)}} className="item-trash gg-trash"></i>
-                        </li>
+                        <>
+                            <li key={product.id} className="clearfix">
+                                <Image width={70} height={70} src={product.image ? `/api/products/image/${product.image}` : `/api/products/image`} alt="Product Image"/>
+                                <span className="item-name">{product.productname}</span>
+                                <span className="item-price">{product.price}</span>
+                                <i onClick={() => {deleteProductToBuy(product)}} className="item-trash gg-trash"></i>
+                            </li>
+                            <hr />
+                        </>
                     )
                 })}
             </ul>
+            <div className="shopping-cart-footer">
+                <div className="shopping-cart-total">
+                    <span className="lighter-text">Total: </span>
+                    <span className="main-color-text">{Number(totalPrice)}</span>
+                </div>
+            </div>
         </div>
     )
 }
